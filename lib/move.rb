bfc9efb04480c87ca56e4c1c3f813e84d36f1116
Kristin Ortiz <itskristin.ortiz@gmail.com>
2017-12-11 22:52:44 +0000

    automatically backed up by learn

--- lib/move.rb
+++ lib/move.rb
@@ -6,6 +6,7 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 puts "Welcome to Tic Tac Toe!"
+board = [" "," "," "," "," "," "," "," "," "]
 display_board(board)
 puts "Where would you like to go? (1 - 9)"
 
